4df4cb31b62fd6768aa531eb17caf8c95b6cb0a8
gzxultra <gladfinger@emails.bjut.edu.cn>
2016-03-13 21:54:02 +0800

    multiplace login complete

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from flask_login import login_user, logout_user, login_required
-from ..models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from ..models import User, OnlineInfo
 from .forms import LoginForm, RegistrationForm
 from .. import db
+from ..email import send_email
+from flask import session, request
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -13,6 +15,11 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
+
+            loginfo = OnlineInfo(username=user.username,
+                                 device=(request.remote_addr + request.user_agent.__str__()))
+            db.session.add(loginfo)
+            db.session.commit()
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
@@ -21,6 +28,10 @@ def login():
 @auth.route('/logout')
 @login_required
 def logout():
+    loginfo = OnlineInfo.query.filter_by(device=(request.remote_addr + request.user_agent.__str__())).first()
+    if loginfo is not None:
+        db.session.delete(loginfo)
+        db.session.commit()
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
@@ -34,9 +45,50 @@ def register():
                     username=form.username.data,
                     password=form.password.data)
         db.session.add(user)
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))
+        db.session.commit()
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                  'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been send to you.')
+        return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
 
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
 
 
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -5,14 +7,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'mygladfinger'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'gzxultra56'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SENDER = '顾志翔 <mygladfinger@gmail.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'gzxultra'
 
     @staticmethod
     def init_app(app):
