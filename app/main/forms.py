0a36a36fa1322e91a35735280bb119c94d016592
gzxultra <gladfinger@emails.bjut.edu.cn>
2016-03-27 17:12:39 +0800

    new feature: pagination widget and markdown support

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,7 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, Regexp, ValidationError
 from ..models import Role, User
+from flask_pagedown.fields import PageDownField
 
 
 class NameForm(Form):
@@ -46,5 +47,5 @@ class EditProfileAdminForm(Form):
 
 
 class PostForm(Form):
-    body = TextAreaField("What's on your mind?", validators=[DataRequired()])
+    body = PageDownField("What's on your mind?", validators=[DataRequired()])
     submit = SubmitField('Submit')
